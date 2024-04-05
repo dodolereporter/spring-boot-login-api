4356ff0118eac48ca5ad69325a9805b723525c21
dodolereporter <dodolereporter@gmail.com>
2024-04-05 18:28:03 +0200

    add import

--- src/main/java/fr/codesbuster/solidstock/api/entity/invoice/InvoiceEntity.java
+++ src/main/java/fr/codesbuster/solidstock/api/entity/invoice/InvoiceEntity.java
@@ -2,6 +2,7 @@ package fr.codesbuster.solidstock.api.entity.invoice;
 
 import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import fr.codesbuster.solidstock.api.entity.CustomerEntity;
+import fr.codesbuster.solidstock.api.entity.OwnerCompanyEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
